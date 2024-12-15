5783d55dab4dee2847c2846d366fbb36c293abff
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-12-15 15:07:02 +0530

    feat : added fabicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./_components/Header";
+import Head from "next/head";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="h-screen">
+      
       <Header />
       <div className="relative flex flex-col gap-5 w-full items-center justify-center overflow-hidden rounded-lg g-background p-4 mt-[12rem]">
         <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
