d93fac39f614351a84337851af0da9ed5fc688b1
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-12-15 13:49:12 +0530

    fix : again some minor change

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -5,19 +5,18 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="p-3 flex items-center shadow-lg justify-between">
-     
-        <div className="flex items-center gap-2">
-          <Image src={"/logo.png"} height={30} width={30} alt="logo" />
-          <h2 className="text-2xl font-bold cursor-pointer">Genius</h2>
-        </div>
-        <div>
-          <Link href={"/dashboard"}>
-            <Button className="bg-violet-600 text-white hover:bg-violet-700">
-              Go to Dashboard
-            </Button>
-          </Link>
-        </div>
+    <div className="p-4 flex items-center shadow-lg justify-between">
+      <div className="flex items-center gap-2">
+        <Image src={"/logo.png"} height={30} width={30} alt="logo" />
+        <h2 className="text-2xl font-bold cursor-pointer">Genius</h2>
+      </div>
+      <div>
+        <Link href={"/dashboard"}>
+          <Button className="bg-violet-600 text-white hover:bg-violet-700">
+            Go to Dashboard
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
